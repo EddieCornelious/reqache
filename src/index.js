306f31a1444d36dded23df2b71394b89e17a1097
Eddie Cornelious <eulerissmart@gmail.com>
2018-12-21 02:42:14 +0000

    init

---
+++ src/index.js
@@ -0,0 +1,31 @@
+
+import {Promise} from 'es6-promise';
+import {fetch as fetchPolly} from 'whatwg-fetch';
+import Json from 'jsonify';
+
+function setFetchResponseType(fetchInstance, type) {
+  return fetchInstance.then(response => {
+    if (!response.ok) {
+      throw Error(response.statusText);
+    }
+    return response[type]();
+  });
+}
+
+export default (url, options) => {
+  let env = options.env || 'dev';
+  let responseType = options.responseType || 'json';
+  const cache = window.localStorage;
+  const request = setFetchResponseType(fetchPolly(url, options), responseType);
+
+  if (env === 'prod') {
+    return request;
+  }
+
+  if (!cache[url]) {
+    request
+      .then(result => cache.setItem(url, Json.stringify(result)));
+  }
+  return Promise.resolve(Json.parse(cache.getItem(url)));
+
+};
